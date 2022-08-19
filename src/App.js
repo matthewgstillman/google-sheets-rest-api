e68e90157de6a6f7af6b650084d2f9aa02c58a74
matthewgstillman <matthewgstillman@gmail.com>
2022-08-18 18:27:48 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import useGoogleSheets from "use-google-sheets";
 
-function App() {
+const App = () => {
+  const { data, loading, error, refetch } = useGoogleSheets({
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    sheetId: process.env.REACT_APP_GOOGLE_SHEETS_ID,
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{JSON.stringify(data)}</div>
+
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
-}
+};
 
 export default App;
